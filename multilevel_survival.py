c6e05c2805479bf745648c88cfd4ed423fde2cb7
pascal <mr.lu.pascal@gmail.com>
2021-06-09 14:49:55 +0200

    update comments

--- multilevel_survival.py
+++ multilevel_survival.py
@@ -11,31 +11,33 @@ def fit(features: np.array,
         num_im: int,
         epsilon_0: float = 0.01,
         n_iter: int = 200) -> np.array:
-    """Fit w in a Cox model h(t) = h_0(t) exp(w*x)
+    """Fit the multilevel survival model
     
     Parameters
     ----------
     features: np.array
-        must be of size num_gen*num_im + 2, where the two last columns are E, T
+        must be of size num_gen*num_im + 2, where the two last columns are delta_i (indicates if event occurs or not), T_i (time to event)
     groups: Dict[int, List[int]]
-        dictionary where keys are genes and values are SNPs associated to gene
+        dictionary where keys are genes and values are SNPs associated to the genes
     penalty1: float
-        lambda_W (must be >= 0)
+        lambda_W (must be >= 0), i.e. the weight of the group lasso penalty
     penalty2: float
-        lambda_I (must be >= 0)
+        lambda_I (must be >= 0), i.e. the weight of the ridge penalty
     num_gen: int
         number of SNPs
     num_im: int
         number of MRI features
     epsilon_0: float
-        must be > 0
+        step, by defaut set to 0.01, must be > 0
     n_iter: int
         number of iterations
     
     Returns
     -------
     w: np.array
-        w = [flatten(W), beta_I]
+        w = [flatten(W), beta_I] where
+        - W is the matrix of interactions between SNPs and imaging data
+        - beta_I is the vector of weights for imaging data
     """
     
     w = np.zeros(features.shape[1]-2)
@@ -48,14 +50,16 @@ def fit(features: np.array,
     index = num_im*num_gen
     
     while True:
+        # stopping criteria: stops if either the number of iterations is reached or epsilon becomes too small
         if i >= n_iter:
             break
         if num_div > 20:
             break
         
+        # gradient descent
         w_new = w - epsilon*gradient(features, w)
         
-        # matrix W
+        # update weights for matrix W
         if penalty1 >= 0:
             for g in range(len(groups)):
                 indices_g = groups[g]
@@ -68,26 +72,29 @@ def fit(features: np.array,
                 else:
                     w_new[indices_g] = W_i_g
         
-        # beta_I
+        # update weights for beta_I
         if penalty2 >= 0:
             threshold2 = epsilon*penalty2
             w_new[index:] = (w_new[index:])/(1+2*epsilon*penalty2)
-        
+
+        # compute new loss for new vectors
         norm_new = penalty2*np.linalg.norm(w_new[index:])**2 + penalty1*np.sum([(np.linalg.norm(w_new[groups[g]])**2)*np.sqrt(len(groups[g])) for g in groups])
         loss_new = log_likelihood(features, w_new) + norm_new
-        
+
+        # check if new loss increases, then choose a smaller step size
         if ((loss_new > loss) | (loss_new == float('nan'))):
             epsilon = epsilon/2
             i += 1
             num_div += 1
             continue
-        
+
+        # update w
         w_prev = w
         w = w_new
         epsilon = epsilon_0
-        
+
+        # stopping criterion if loss does not change
         res = np.abs(1 - (loss_new / loss))
-        
         if (res < 1e-20):
             break
 
@@ -101,7 +108,7 @@ def fit(features: np.array,
 
 
 def scaler(features: np.array) -> Tuple[np.array, np.array, np.array]:
-    """Scaler
+    """Z-score scaling of features
         
     Parameters
     ----------
@@ -125,9 +132,50 @@ def scaler(features: np.array) -> Tuple[np.array, np.array, np.array]:
     return scaled_features, mean, std
 
 
+def log_likelihood(features: np.array,
+                   beta: np.array) -> float:
+    """Compute log-likelihood in a Cox model
+        h(t) = h_0(t) exp(beta*x)
+    
+    Parameters
+    ----------
+    features: np.array
+        The two last columns are E, T
+    beta: np.array
+        Vector where the log-likelihood is computed
+        
+    Returns
+    -------
+    loss: float
+        log-likelihood
+    """
+    
+    time = features[:, -1]
+    o = np.argsort(-time, kind="mergesort")
+    features_ = features[o, :-2]
+    T = np.array([features[o, -1]]).transpose()
+    E = np.array([features[o, -2]]).transpose()
+    betax = np.dot(features_, beta)
+    n_samples, _ = features_.shape
+    
+    loss = 0
+    risk_set = 0
+    k = 0
+    
+    for i in range(n_samples):
+        ti = T[i,0]
+        while k < n_samples and ti == T[k,0]:
+            risk_set += np.exp(betax[k])
+            k += 1
+        
+        if E[i,0]:
+            loss -= (betax[i] - np.log(risk_set))/n_samples
+    return loss
+
+
 def gradient(features: np.array,
              beta: np.array) -> np.array:
-    """Compute log-likelihood gradient in a Cox model with respect to beta.
+    """Compute gradient of log-likelihood in a Cox model with respect to beta.
     h(t) = h_0(t) exp(beta*x)
         
     Parameters
@@ -175,46 +223,4 @@ def gradient(features: np.array,
             gradient -= (x[i:i + 1] - risk_set_x / risk_set) * inv_n_samples
 
     return gradient.ravel()
-
-
-
-def log_likelihood(features: np.array,
-                   beta: np.array) -> float:
-    """Compute log-likelihood in a Cox model with respect to beta.
-    h(t) = h_0(t) exp(beta*x)
-        
-    Parameters
-    ----------
-    features: np.array
-        The two last columns are E, T
-    beta: np.array
-        Vector where the log-likelihood is computed
-        
-    Returns
-    -------
-    loss: float
-        log-likelihood
-    """
-    
-    time = features[:, -1]
-    o = np.argsort(-time, kind="mergesort")
-    features_ = features[o, :-2]
-    T = np.array([features[o, -1]]).transpose()
-    E = np.array([features[o, -2]]).transpose()
-    betax = np.dot(features_, beta)
-    n_samples, _ = features_.shape
-    
-    loss = 0
-    risk_set = 0
-    k = 0
-    
-    for i in range(n_samples):
-        ti = T[i,0]
-        while k < n_samples and ti == T[k,0]:
-            risk_set += np.exp(betax[k])
-            k += 1
-        
-        if E[i,0]:
-            loss -= (betax[i] - np.log(risk_set))/n_samples
-    return loss
 
--- readme.md
+++ readme.md
@@ -1,13 +1,19 @@
 # Multilevel Survival Modeling with Structured Penalties for Disease Prediction from Imaging Genetics data
 
-This code is a complement to the corresponding article. It has been tested which Python 3.8 and Numpy 1.18.5 on a MacBook Pro (Retina, 13-inch, Late 2013).
+This code corresponds to the multilevel survival modelling approach proposed in the following paper:
+
+> Lu P and Colliot O, Multilevel Survival Modeling with Structured Penalties for Disease Prediction from Imaging Genetics data, *Submitted to IEEE Journal of Biomedical and Health Informatics*, 2021.
+
+It is written in Python and the only dependency is Numpy. This code is thus expected to work in most environments even though it has been developed with Python 3.8 and Numpy 1.18.5 on a MacBook Pro (Retina, 13-inch, Late 2013).
 
 ## Structure of the code
 
 The code is mainly composed of the fit method:
-* `fit`: Fit w in a Cox model h(t) = h_0(t) exp(w*x)
+* `fit`: Fits the multilevel survival model.
 
 and three other helper methods:
-* `scaler`: Scales features matrix (without censorship E and time of event T).
-* `gradient`: Compute log-likelihood gradient in a Cox model with respect to w.
-* `log_likelihood`: Compute log-likelihood in a Cox model with respect to w.
+* `scaler`: Scales the feature matrix.
+* `gradient`: Compute the log-likelihood gradient.
+* `log_likelihood`: Compute the log-likelihood.
+
+Please refer to the comments of each function for details about their specific arguments.
